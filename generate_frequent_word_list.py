836ffc28d95f36b55392f53b3bbe47e5ee53e052
GJA <gja@zurich.ibm.com>
2018-04-02 09:48:01 +0200

    script to gen a list of frquent words in a corpus

---
+++ generate_frequent_word_list.py
@@ -0,0 +1,22 @@
+import preprocess_helper
+
+
+class Config:
+
+    def __init__(self):
+        # default constructor
+        self.input_file_name = 'data/sentences.train'
+        self.k_frequent_words = 20000 - 4
+        self.frequent_words_file_name = 'data/k_frequent_words.txt'
+
+
+def main():
+    config = Config()
+    top_k_words = preprocess_helper.generate_top_k_words(config.input_file_name, config.k_frequent_words)
+    top_k_words.extend(['<unk>', '<pad>', '<bos>', '<eos>'])
+    print(top_k_words)
+    preprocess_helper.write_list_to_file(top_k_words, config.frequent_words_file_name)
+
+
+if __name__ == "__main__":
+    main()
