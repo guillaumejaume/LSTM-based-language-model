adf8cc61e85863d17b97791ebfb2f2269aed2451
Anca-Nicoleta Ciubotaru <aci@zurich.ibm.com>
2018-04-12 17:12:43 +0200

    Add word embeddings for <bos> <eos> <pad> <unk>

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ A language model implementation in Tensorflow using Long-Short-Term-Memory (LSTM
 - Word2Vec data available at: https://polybox.ethz.ch/index.php/s/cpicEJeC2G4tq9U
 
 #### Output examples 
+
+
+
+#### Install @Leonard
+module load python_gpu/3.6.1 cuda/9.0.176 cudnn/7.0
+pip install --user pipenv
+pipenv install numpy
+pipenv install tensorflow-gpu==1.6
+pipenv install gensim
+pipenv shell #To activate this project's virtualenv.  Use 'exit' to leave.
--- generate_frequent_word_list.py
+++ generate_frequent_word_list.py
@@ -5,16 +5,16 @@ class Config:
 
     def __init__(self):
         # default constructor
-        self.input_file_name = 'data/sentences.train'
-        self.k_frequent_words = 20000 - 4
-        self.frequent_words_file_name = 'data/k_frequent_words.txt'
+        self.input_file_name = 'data/sentences_small.train'
+        self.k_frequent_words = 100 - 4
+        self.frequent_words_file_name = 'data/k_frequent_words.word2vec'
+        self.embedding_dimension = 100
 
 
 def main():
     config = Config()
-    top_k_words = preprocess_helper.generate_top_k_words(config.input_file_name, config.k_frequent_words)
-    top_k_words.extend(['<unk>', '<pad>', '<bos>', '<eos>'])
-    preprocess_helper.write_list_to_file(top_k_words, config.frequent_words_file_name)
+    top_k_words_and_embedding_pairs = preprocess_helper.generate_top_k_words_and_their_emb(config.input_file_name, config.k_frequent_words, config.embedding_dimension)
+    preprocess_helper.write_list_to_file(top_k_words_and_embedding_pairs, config.frequent_words_file_name)
 
 
 if __name__ == "__main__":
